9fa60e3a186eb396c8e26cbbdf8ffb0fbe85d5c8
MhfZuu <muhammadhanifzuhair2005@mail.ugm.ac.id>
2026-08-05 12:22:28 +0700

    fix: mixed content: trust Railway proxy for HTTPS detection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Percaya proxy Railway supaya Laravel tahu request aslinya HTTPS
+        // (fix mixed content: asset()/fetch() generate http:// padahal halaman https://)
+        $middleware->trustProxies(
+            at: '*',
+            headers: Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+                Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+                Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+                Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
+                Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
